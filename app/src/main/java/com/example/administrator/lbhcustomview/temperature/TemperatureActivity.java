6cf67c56631d9cdae9db4901db263c68b1c63f6f
lbhlc <76681287@qq.com>
2017-12-29 10:58:33 +0800

    添加了自定义温度计页面

---
+++ app/src/main/java/com/example/administrator/lbhcustomview/temperature/TemperatureActivity.java
@@ -0,0 +1,40 @@
+package com.example.administrator.lbhcustomview.temperature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.example.administrator.lbhcustomview.R;
+import com.example.lbhlibrary.customview.temperature.TemperatureView;
+
+public class TemperatureActivity extends AppCompatActivity {
+    private TemperatureView temperatureView;
+    private SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature);
+        temperatureView = (TemperatureView) findViewById(R.id.temperatureView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        temperatureView.start(-20);
+       // seekBar.setProgress(80 + 40);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                temperatureView.start(progress );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
